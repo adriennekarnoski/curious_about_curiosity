e1d0b43e071bd9327d7253182bc0c890f4b74a37
Darryl Davis <ddavis1306@gmail.com>
2017-09-05 16:08:11 -0700

    calendar in mmmm-dd format

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Load Express
-const pg = require('pg');
+// const pg = require('pg');
 const express = require('express');
 const requestProxy = require('express-request-proxy');
 
@@ -8,18 +8,18 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static('./public'));
-
+//
 app.listen(PORT, function() {
   console.log('Our app is listening on port ' + PORT);
 });
-
-function proxyNasa(request, response) {
-  console.log('Routing GitHub request for', request.params[0]);
-  (proxyNasa({
-    url: `https://api.nasa.gov/${request.params[0]}`,
-    headers: {Authorization: `token ${process.env.NASA_TOKEN}`}
-  }))(request, response);
-}
-
-app.get('/api.nasa.gov/*', proxyNasa);
-app.get('*', (request, response) => response.sendFile('index.html', {root: './public'}));
+//
+// function proxyNasa(request, response) {
+//   console.log('Routing GitHub request for', request.params[0]);
+//   (proxyNasa({
+//     url: `https://api.nasa.gov/${request.params[0]}`,
+//     headers: {Authorization: `token ${process.env.NASA_TOKEN}`}
+//   }))(request, response);
+// }
+//
+// app.get('/api.nasa.gov/*', proxyNasa);
+// app.get('*', (request, response) => response.sendFile('index.html', {root: './public'}));
