9a6c7712f149701dd1a1bfe9226f04bab5ac3067
Darryl Davis <ddavis1306@gmail.com>
2017-09-07 08:07:11 -0700

    inner rows have backgrounds and moving background is almost functional

--- public/scripts/controllers/roverController.js
+++ public/scripts/controllers/roverController.js
@@ -16,5 +16,19 @@ var app = app || {};
     // app.Curiosity.requestData(`${yyyy}-${mm}-${dd}`); // live date - (current date onload)
   };
 
+  $(document).ready(function() {
+  var movementStrength = 25;
+  var height = movementStrength / $(window).height();
+  var width = movementStrength / $(window).width();
+  $(".command-view").mousemove(function(e){
+            var pageX = e.pageX - ($(window).width() / 2);
+            var pageY = e.pageY - ($(window).height() / 2);
+            var newvalueX = width * pageX * -1 - 25;
+            var newvalueY = height * pageY * -1 - 50;
+            $('.command-view').css("background-position", newvalueX+"px     "+newvalueY+"px");
+    console.log('working');
+  });
+  });
+
   module.roverController = roverController;
 })(app);
